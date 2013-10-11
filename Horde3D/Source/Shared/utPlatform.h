5037e86802bc481595209391d05a0f47a6277962
Cloud Wu <cloudwu@gmail.com>
2013-10-11 11:21:36 +0800

    check _MSC_VER for vs solustion files

--- Horde3D/Source/Shared/utPlatform.h
+++ Horde3D/Source/Shared/utPlatform.h
@@ -74,6 +74,9 @@ typedef unsigned long long uint64;
 #undef max
 #endif
 
+#if defined( _MSC_VER ) && (_MSC_VER >= 1400) && !defined(HAVE_STRNCPY_S)
+#	define HAVE_STRNCPY_S
+#endif
 #if !defined( HAVE_STRNCPY_S )
 #	define strncpy_s( dst, dstSize, src, count ) strncpy( dst, src, count < dstSize ? count : dstSize )
 #endif
