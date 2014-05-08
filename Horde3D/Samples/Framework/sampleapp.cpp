1a830b13fe331f548e4eca40190c6b5bc8bbcbf8
Jean-Claude Monnin <jc_monnin@emailplus.org>
2014-05-08 20:56:18 +0200

    Remove unused stencil buffer

--- Horde3D/Samples/Framework/sampleapp.cpp
+++ Horde3D/Samples/Framework/sampleapp.cpp
@@ -586,7 +586,6 @@ bool SampleApplication::init()
     glfwWindowHint( GLFW_BLUE_BITS, 8 );
     glfwWindowHint( GLFW_ALPHA_BITS, 8 );
     glfwWindowHint( GLFW_DEPTH_BITS, 24 );
-    glfwWindowHint( GLFW_STENCIL_BITS, 8 );
     glfwWindowHint( GLFW_SAMPLES, _winSampleCount );
 
     if ( _winFullScreen ) {
