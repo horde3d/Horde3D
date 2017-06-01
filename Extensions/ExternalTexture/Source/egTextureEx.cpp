672b0fc681221edd5b3ba7502842e03bc2af7147
Volker Vogelhuber <wiendl@web.de>
2017-06-01 20:45:40 +0200

    Added Extension to allow replacing a Horde3D allocated texture by an external OpenGL texture for binding external rendering engines

---
+++ Extensions/ExternalTexture/Source/egTextureEx.cpp
@@ -0,0 +1,84 @@
+// *************************************************************************************************
+//
+// Horde3D ExternalTexture Extension
+// --------------------------------------------------------
+// Copyright (C) 2017 Volker Vogelhuber
+//
+// This software is distributed under the terms of the Eclipse Public License v1.0.
+// A copy of the license may be obtained at: http://www.eclipse.org/legal/epl-v10.html
+//
+// *************************************************************************************************
+
+#include "egTextureEx.h"
+
+#include <egModules.h>
+#include <egCom.h>
+#include <egRenderer.h>
+#include <utOpenGL.h>
+
+#include "egRendererBaseGL2.h"
+#include "egRendererBaseGL4.h"
+
+namespace  Horde3DExternalTexture {
+
+using namespace Horde3D;
+
+TextureResourceEx::TextureResourceEx(const std::string &name, int flags) : TextureResource(name,flags), m_imported(false)
+{
+}
+
+void TextureResourceEx::release()
+{
+    if( m_imported )
+        replaceTexObj(0);
+    TextureResource::release();
+}
+
+bool TextureResourceEx::importTexGL(int texGL, int width, int height )
+{
+    _loaded = true;
+    _width = width;
+    _height = height;
+
+    if( _texObject == defTex2DObject )
+    {
+        m_imported = false;
+        RenderDeviceInterface *rdi = Modules::renderer().getRenderDevice();
+        _texObject = rdi->createTexture( _texType, _width, _height, _depth, _texFormat,
+                                         false, false, false, _sRGB );
+    }
+    replaceTexObj( texGL );
+    m_imported = true;
+}
+
+void TextureResourceEx::replaceTexObj( int texObj )
+{
+    switch ( Modules::renderer().getRenderDeviceType() )
+    {
+    case RenderBackendType::OpenGL2:
+    {
+        RDI_GL2::RenderDeviceGL2 *rdi = ( RDI_GL2::RenderDeviceGL2 * ) Modules::renderer().getRenderDevice();
+        RDI_GL2::RDITextureGL2& tex = rdi->getTexture( _texObject );
+        if( !m_imported && tex.glObj )
+            glDeleteTextures( 1, &tex.glObj );
+        tex.glObj = texObj;
+        tex.memSize = 0;
+        return;
+    }
+    case RenderBackendType::OpenGL4:
+    {
+        RDI_GL4::RenderDeviceGL4 *rdi = ( RDI_GL4::RenderDeviceGL4 * ) Modules::renderer().getRenderDevice();
+        RDI_GL4::RDITextureGL4& tex = rdi->getTexture( _texObject );
+        if( !m_imported && tex.glObj )
+            glDeleteTextures( 1, &tex.glObj );
+        tex.glObj = texObj;
+        tex.memSize = 0;
+        return;
+    }
+    default:
+        Modules::log().writeError("Render backend not supported");
+        return;
+    }
+}
+
+} // Namespace
---
+++ Extensions/ExternalTexture/Source/extension.h
@@ -0,0 +1,32 @@
+// *************************************************************************************************
+//
+// Horde3D ExternalTexture Extension
+// --------------------------------------------------------
+// Copyright (C) 2017 Volker Vogelhuber
+//
+// This software is distributed under the terms of the Eclipse Public License v1.0.
+// A copy of the license may be obtained at: http://www.eclipse.org/legal/epl-v10.html
+//
+// *************************************************************************************************
+
+#ifndef _Horde3DExternalTexture_extension_H_
+#define _Horde3DExternalTexture_extension_H_
+
+#include "egPrerequisites.h"
+#include "egExtensions.h"
+
+
+namespace Horde3DExternalTexture {
+
+
+class ExternalTexture : public Horde3D::IExtension
+{
+public:
+    virtual const char *getName() const { return "ExternalTexture"; }
+	virtual bool init();
+	virtual void release();
+};
+
+}
+
+#endif	// _Horde3DExternalTexture_extension_H_
