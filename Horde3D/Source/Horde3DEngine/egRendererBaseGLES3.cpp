b28ea3b3fc5f6059a56bfa922b2a645f773a588d
Irdis <algts@inbox.ru>
2020-11-07 18:13:40 +0300

    Fix drawing with patches (fixes tessellation on opengl es).

--- Horde3D/Source/Horde3DEngine/egRendererBaseGLES3.cpp
+++ Horde3D/Source/Horde3DEngine/egRendererBaseGLES3.cpp
@@ -51,7 +51,7 @@ static const char *defaultShaderFS =
 
 static const uint32 indexFormats[ 2 ] = { GL_UNSIGNED_SHORT, GL_UNSIGNED_INT };
 
-static const uint32 primitiveTypes[ 5 ] = { GL_TRIANGLES, GL_TRIANGLE_STRIP, GL_LINES, GL_POINTS, 0 };
+static const uint32 primitiveTypes[ 5 ] = { GL_TRIANGLES, GL_TRIANGLE_STRIP, GL_LINES, GL_POINTS, GL_PATCHES };
 
 static const uint32 textureTypes[ 3 ] = { GL_TEXTURE_2D, GL_TEXTURE_3D, GL_TEXTURE_CUBE_MAP };
 
