5c6e15b2bd4282a93a11d187bac16f018c73be58
Irdis <algts@inbox.ru>
2021-10-23 20:41:33 +0300

    Added some tests on parsing samplers from binary shader

--- Horde3D/Tests/unit/test_shaderparser.cpp
+++ Horde3D/Tests/unit/test_shaderparser.cpp
@@ -1,9 +1,14 @@
 #include "../catch.hpp"
 #include <memory>
+#include <iostream>
 
 #include "../../Source/Horde3DEngine/egShaderParser.h"
+#include "../../Source/Horde3DEngine/egModules.h"
+#include "../../Source/Horde3DEngine/egRenderer.h"
 
 using namespace Horde3D;
+
+#define NULL_RENDER_BACKEND 256
 
 class TestShaderParser : public ShaderParser
 {
@@ -11,7 +16,16 @@ public:
     
     TestShaderParser( const std::string shaderName ) : ShaderParser( shaderName )
     {
-        
+        // Init engine
+        _initialized = Modules::init( NULL_RENDER_BACKEND );
+        if ( !_initialized )
+            std::cout << "---Engine failed to initialize. Test will likely fail/crash!---" << std::endl;
+    }
+    
+    ~TestShaderParser()
+    {
+        if ( _initialized )
+            Modules::release();
     }
     
     bool test_parseBinarySampler( char *data, uint32_t samplerCount )
@@ -23,17 +37,24 @@ public:
     {
         return parseBinaryUniforms( data, variablesCount );
     }
+    
+private:
+    
+    bool _initialized;
 };
 
 enum class SamplerData
 {
     Incorrect_Type,
+    Incorrect_Id,
+    Incorrect_TexID,
     Correct
 };
 
 uint8_t *generateBinarySamplerData( SamplerData genType, int iteration )
 {
     uint8_t *data = new uint8_t[ 1024 ];
+    memset( data, 0, 1024 );
     
     switch( genType )
     {
@@ -46,6 +67,36 @@ uint8_t *generateBinarySamplerData( SamplerData genType, int iteration )
             if ( iteration == 2 ) samplerData[ 0 ] = 4; 
             if ( iteration == 3 ) samplerData[ 0 ] = 7; 
 
+            break;
+        }
+        case SamplerData::Incorrect_Id:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            
+            if ( iteration == 0 ) samplerData[ 1 ] = 0;
+            if ( iteration == 1 ) 
+            {
+                samplerData[ 1 ] = 257;
+                samplerData[ 2 ] = 'a';
+            }
+            
+            break;
+        }
+        case SamplerData::Incorrect_TexID:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            
+            if ( iteration == 0 ) samplerData[ 4 ] = 0;
+            if ( iteration == 1 ) 
+            {
+                samplerData[ 4 ] = 257;
+                samplerData[ 5 ] = 'a';
+            }
             break;
         }
         case SamplerData::Correct:
@@ -95,4 +146,31 @@ TEST_CASE( "parse binary sampler", "[unit-shader]" )
         samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Type, 3 ) );
         REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
     }
+    
+    SECTION( "incorrect sampler id" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Id, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Id, 1 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+    }
+    
+    SECTION( "incorrect sampler texture id" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexID, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexID, 1 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+    }
+    
+//     SECTION( "incorrect sampler texunit" )
+//     {
+//         samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexID, 0 ) );
+//         REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+//         
+//         samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexID, 1 ) );
+//         REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+//     }
 }
