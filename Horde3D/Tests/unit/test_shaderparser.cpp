db60f7c1fd8c2cb7472a9a5d4af7c241bc30698e
Irdis <algts@inbox.ru>
2021-10-24 20:17:06 +0300

    Tests for sampler parsing in binary shaders should be done.

--- Horde3D/Tests/unit/test_shaderparser.cpp
+++ Horde3D/Tests/unit/test_shaderparser.cpp
@@ -5,6 +5,7 @@
 #include "../../Source/Horde3DEngine/egShaderParser.h"
 #include "../../Source/Horde3DEngine/egModules.h"
 #include "../../Source/Horde3DEngine/egRenderer.h"
+#include "../../Source/Horde3DEngine/egRendererBaseNull.h"
 
 using namespace Horde3D;
 
@@ -48,6 +49,11 @@ enum class SamplerData
     Incorrect_Type,
     Incorrect_Id,
     Incorrect_TexID,
+    Incorrect_TexUnit,
+    Incorrect_Address,
+    Incorrect_Filter,
+    Incorrect_Aniso,
+    Incorrect_Usage,
     Correct
 };
 
@@ -91,16 +97,140 @@ uint8_t *generateBinarySamplerData( SamplerData genType, int iteration )
             samplerData[ 2 ] = 'a'; // sampler id
             samplerData[ 3 ] = 'b';
             
-            if ( iteration == 0 ) samplerData[ 4 ] = 0;
+            if ( iteration == 0 ) samplerData[ 4 ] = 0; // sampler tex id size
             if ( iteration == 1 ) 
             {
-                samplerData[ 4 ] = 257;
-                samplerData[ 5 ] = 'a';
+                samplerData[ 4 ] = 257; // sampler tex id size
+                samplerData[ 5 ] = 'a'; // sampler tex id
+            }
+            break;
+        }
+        case SamplerData::Incorrect_TexUnit:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            samplerData[ 4 ] = 4; // sampler tex id size
+            samplerData[ 5 ] = 'c'; // sampler tex id
+            samplerData[ 6 ] = 'd';
+            
+            if ( iteration == 0 ) 
+            {
+                short *d = ( short * ) samplerData;
+                d[ 7 ] = -1;
+            }
+            if ( iteration == 1 )
+            {
+                short *d = ( short * ) samplerData;
+                d[ 7 ] = 16;
+            }
+            break;
+        }
+        case SamplerData::Incorrect_Address:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            samplerData[ 4 ] = 4; // sampler tex id size
+            samplerData[ 5 ] = 'c'; // sampler tex id
+            samplerData[ 6 ] = 'd';
+            samplerData[ 7 ] = 0; // sampler texunit
+            samplerData[ 8 ] = 3; // sampler address
+            
+            break;
+        }
+        case SamplerData::Incorrect_Filter:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            samplerData[ 4 ] = 4; // sampler tex id size
+            samplerData[ 5 ] = 'c'; // sampler tex id
+            samplerData[ 6 ] = 'd';
+            samplerData[ 7 ] = 0; // sampler texunit
+            samplerData[ 8 ] = 0; // sampler address
+            samplerData[ 9 ] = 3; // sampler filter
+            
+            break;
+        }
+        case SamplerData::Incorrect_Aniso:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            samplerData[ 4 ] = 4; // sampler tex id size
+            samplerData[ 5 ] = 'c'; // sampler tex id
+            samplerData[ 6 ] = 'd';
+            samplerData[ 7 ] = 0; // sampler texunit
+            samplerData[ 8 ] = 0; // sampler address
+            samplerData[ 9 ] = 2; // sampler filter
+            
+            if ( iteration == 0 ) samplerData[ 10 ] = 0;
+            if ( iteration == 1 ) samplerData[ 10 ] = 3;
+            if ( iteration == 2 ) samplerData[ 10 ] = 5;
+            if ( iteration == 3 ) samplerData[ 10 ] = 6;
+            if ( iteration == 4 ) samplerData[ 10 ] = 7;
+            if ( iteration == 5 ) samplerData[ 10 ] = 9;
+            if ( iteration == 6 ) samplerData[ 10 ] = 10;
+            if ( iteration == 7 ) samplerData[ 10 ] = 11;
+            if ( iteration == 8 ) samplerData[ 10 ] = 12;
+            if ( iteration == 9 ) samplerData[ 10 ] = 13;
+            if ( iteration == 10 ) samplerData[ 10 ] = 14;
+            if ( iteration == 11 ) samplerData[ 10 ] = 15;
+            if ( iteration == 12 ) samplerData[ 10 ] = 17;
+
+            break;
+        }
+        case SamplerData::Incorrect_Usage:
+        {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            samplerData[ 4 ] = 4; // sampler tex id size
+            samplerData[ 5 ] = 'c'; // sampler tex id
+            samplerData[ 6 ] = 'd';
+            samplerData[ 7 ] = 0; // sampler texunit
+            samplerData[ 8 ] = 0; // sampler address
+            samplerData[ 9 ] = 2; // sampler filter
+            samplerData[ 10 ] = 1; // sampler anisotropy
+            
+            if ( iteration == 0 ) samplerData[ 11 ] = 4;
+            if ( iteration == 1 ) 
+            {
+                samplerData[ 0 ] = 3; // sampler type
+                samplerData[ 11 ] = 3; // sampler usage
+            }
+            if ( iteration == 2 )
+            {
+                samplerData[ 11 ] = 2; // samplerUsage
             }
             break;
         }
         case SamplerData::Correct:
         {
+            uint16_t *samplerData = ( uint16_t * ) data;
+            samplerData[ 0 ] = 2; // sampler2D
+            samplerData[ 1 ] = 4; // sampler id size
+            samplerData[ 2 ] = 'a'; // sampler id
+            samplerData[ 3 ] = 'b';
+            samplerData[ 4 ] = 4; // sampler tex id size
+            samplerData[ 5 ] = 'c'; // sampler tex id
+            samplerData[ 6 ] = 'd';
+            samplerData[ 7 ] = 0; // sampler texunit
+            samplerData[ 8 ] = 0; // sampler address
+            samplerData[ 9 ] = 2; // sampler filter
+            samplerData[ 10 ] = 8; // sampler anisotropy
+            samplerData[ 11 ] = 0; // sampler usage
             break;
         }
         default:
@@ -165,12 +295,91 @@ TEST_CASE( "parse binary sampler", "[unit-shader]" )
         REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
     }
     
-//     SECTION( "incorrect sampler texunit" )
-//     {
-//         samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexID, 0 ) );
-//         REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
-//         
-//         samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexID, 1 ) );
-//         REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
-//     }
+    SECTION( "incorrect sampler texunit" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexUnit, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );        
+
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_TexUnit, 1 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) ); 
+    }
+    
+    SECTION( "incorrect sampler address" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Address, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+    }
+
+    SECTION( "incorrect sampler filter" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Filter, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+    }
+    
+    SECTION( "incorrect sampler anisotropy" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );        
+
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 1 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 2 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 3 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 4 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 5 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 6 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 7 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 8 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 9 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 10 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 11 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Aniso, 12 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+    }
+    
+    SECTION( "incorrect sampler usage" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Usage, 0 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Usage, 1 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+        // modify render device caps to handle case when computer shaders are not available
+        RDI_Null::RenderDeviceNull *rdi = ( RDI_Null::RenderDeviceNull * ) Modules::renderer().getRenderDevice();
+        DeviceCaps caps = rdi->getCaps();
+        caps.computeShaders = false;
+        rdi->setCaps( caps );
+            
+        samplerData.reset( generateBinarySamplerData( SamplerData::Incorrect_Usage, 2 ) );
+        REQUIRE_FALSE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) );
+        
+    }
+    
+    SECTION( "correct usage" )
+    {
+        samplerData.reset( generateBinarySamplerData( SamplerData::Correct, 0 ) );
+        REQUIRE( p.test_parseBinarySampler( (char *) samplerData.get(), 1 ) == true );        
+    }
 }
