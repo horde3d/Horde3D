72004652e1f72f47e5bc601e884227b5e3bf6b3b
jesse <jessevandis@gmail.com>
2024-08-09 22:18:53 +0200

    cleanup: removed docker for testing

--- build_emscripten.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-mkdir ./build 2>/dev/null
-
-docker build -t build_with_emscripten .
-
-docker run --rm -u $(id -u):$(id -g) -v $(pwd):/mount:ro -v $(pwd)/build:/build build_with_emscripten
-
