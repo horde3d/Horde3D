4fbea2641e85d825927afd51abceb142aeba820e
jesse <jessevandis@gmail.com>
2025-08-14 20:15:23 +0200

    cherrypick 01e579f from master

--- Horde3D/Source/Shared/utPlatform.h
+++ Horde3D/Source/Shared/utPlatform.h
@@ -18,10 +18,10 @@
 #if !defined( NDEBUG )
 	#include <assert.h>
 #endif
-#if defined(__MINGW32__)
-	#include <math.h>
+#if defined(__MINGW32__) || (__GNUC__ >= 14)
+    #include <math.h>
+    #include <cstdint>
 #endif
-#include <cinttypes>
 
 // Detect platform
 #if defined( WINCE )
